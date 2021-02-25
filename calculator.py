aaf373d20d21baa7f71b8d8b073b66b9b56cce9f
Missy Davies <ms.melissadavies@gmail.com>
2021-02-25 20:08:07 +0000

    Added calculator and pycache files

--- calculator.py
+++ calculator.py
@@ -3,3 +3,12 @@
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
+while True:
+    equation = input("Enter your equation here > ")
+    equation_list = equation.split(" ")
+
+    if equation_list[0] == "q":
+        break
+    elif equation_list[0] == "+":
+        print(add(equation_list[1], equation_list[2]))
+
