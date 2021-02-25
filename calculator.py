56dcda73cc0ff55d6b933d67ca5ba1ba21eeae07
Missy Davies <ms.melissadavies@gmail.com>
2021-02-25 20:51:51 +0000

    Edited instructions

--- calculator.py
+++ calculator.py
@@ -5,17 +5,17 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 print("""INSTRUCTIONS:
 Hi and welcome to my super prefix calculator!
-Please enter your equation when prompted using one of the following operations:
-+ : for addition of two numbers
-- : for subtraction of two numbers 
-* : for multiplication of two numbers
-/ : for division of two numbers 
-square : to get the square of one number
-cube : to get the cube of one number
-power : to get one number raised to the power of a second number 
-mod : to get the remainder of one number divided by a second number 
-x+ : to get (num1 + num2) * num3
-cubes+ : to take two numbers, then add together the cubes of those numbers
+Please enter your equation when prompted using the proper notation for the following operations:
++ 1 2 : for addition of two numbers
+- 1 2: for subtraction of two numbers 
+* 1 2: for multiplication of two numbers
+/ 1 2: for division of two numbers 
+square 1: to get the square of one number
+cube 1: to get the cube of one number
+power 1 2: to get one number raised to the power of a second number 
+mod 1 2: to get the remainder of one number divided by a second number 
+x+ 1 2 3: to get (num1 + num2) * num3
+cubes+ 1 2: to take two numbers, then add together the cubes of those numbers
 """)
 
 while True:
