0886b58e43da72267f89dccf6482047d859aa9f7
Missy Davies <ms.melissadavies@gmail.com>
2021-02-25 20:49:53 +0000

    Added instructions for how to use the calculator

--- calculator.py
+++ calculator.py
@@ -3,6 +3,21 @@
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, add_mult, add_cubes)
 
+print("""INSTRUCTIONS:
+Hi and welcome to my super prefix calculator!
+Please enter your equation when prompted using one of the following operations:
++ : for addition of two numbers
+- : for subtraction of two numbers 
+* : for multiplication of two numbers
+/ : for division of two numbers 
+square : to get the square of one number
+cube : to get the cube of one number
+power : to get one number raised to the power of a second number 
+mod : to get the remainder of one number divided by a second number 
+x+ : to get (num1 + num2) * num3
+cubes+ : to take two numbers, then add together the cubes of those numbers
+""")
+
 while True:
     equation = input("Enter your equation here > ")
     equation_list = equation.split(" ")
